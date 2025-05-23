c7114244990bcb0b3c7c6f03eda4d7b0b8912a2f
Ivan Lučin <lucin.ivan@gmail.com>
2025-05-23 20:37:07 +0200

    Fix viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,7 +72,7 @@ export default function RootLayout({
   return (
     <html lang="hr">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
       </head>
       <body className={`${ttRamillas.variable} antialiased`}>
         {children}
